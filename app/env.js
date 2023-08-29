c4816aebed74cf70aea9a308614929528830a0c4
Tyler Marcott <tylerjmarcott@gmail.com>
2023-08-29 15:03:17 -0600

    init commit

--- app/env.js
+++ app/env.js
@@ -6,3 +6,4 @@ export const audience = 'https://codeworksclassroom.com'
 export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
 
 
+// NOTE: env file uploads the auth 0 log in stuff we need in order to get user access to the BCW API
---
+++ app/models/House.js
@@ -0,0 +1,17 @@
+
+
+
+export class House {
+  constructor(data) {
+    this.id = data.id
+    this.bedrooms = data.bedrooms
+    this.bathrooms = bathrooms
+    this.levels = data.levels
+    this.imgUrl = data.imgUrl
+    this.year = data.year
+    this.price = data.price
+    this.description = data.description
+    this.creatorId = data.creatorId
+    this.creator = data.creator
+  }
+}
